d24e7ec391a01332d136d4771dc2be56aa2313db
AKSHAY <akshayjadhav461999@gmail.com>
2020-08-31 17:42:13 +0530

    new field added in infoBox

--- src/components/InfoBox/InfoBox.js
+++ src/components/InfoBox/InfoBox.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./InfoBox.css";
 import { Card, CardContent, Typography } from "@material-ui/core";
-function InfoBox({ title, cases }) {
+function InfoBox({ title, cases, newCases }) {
   return (
     <div className="infoBox">
-      <Card>
+      <Card className="infoBox__card">
         <CardContent>
           <Typography color="textSecondary" className="infoBox__title">
             {title}
           </Typography>
           <h2 className="infoBox__cases">{cases}</h2>
+          {newCases && (
+            <Typography color="textSecondary" className="infoBox__title">
+              {newCases} new
+            </Typography>
+          )}
         </CardContent>
       </Card>
     </div>
--- src/components/StatusPanal/StatusPanal.js
+++ src/components/StatusPanal/StatusPanal.js
@@ -5,9 +5,17 @@ import InfoBox from "../InfoBox/InfoBox";
 function StatusPanal({ countryInfo }) {
   return (
     <div className="statusPanal">
-      <InfoBox title="Active Cases" cases={countryInfo.cases?.active} />
+      <InfoBox
+        title="Active Cases"
+        cases={countryInfo.cases?.active}
+        newCases={countryInfo.cases?.new}
+      />
       <InfoBox title="Total Recoverd" cases={countryInfo.cases?.recovered} />
-      <InfoBox title="Total Deaths" cases={countryInfo.deaths?.total} />
+      <InfoBox
+        title="Total Deaths"
+        cases={countryInfo.deaths?.total}
+        newCases={countryInfo.deaths?.new}
+      />
       <InfoBox title="Total cases" cases={countryInfo.cases?.total} />
     </div>
   );
