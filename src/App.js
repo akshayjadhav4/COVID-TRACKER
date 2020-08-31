6796a35d33bb4259a1437075c959e8b69daec7f2
AKSHAY <akshayjadhav461999@gmail.com>
2020-08-31 13:17:49 +0530

    AppHeader done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-
+import AppHeader from "./components/AppHeader/AppHeader";
 function App() {
-  return <div className="app"></div>;
+  //state to store list of countries affected by the Coronavirus
+  const [countries, setCountries] = useState([]);
+  //state for dropdown select item
+  const [country, setCountry] = useState("worldWide");
+
+  useEffect(() => {
+    // getting list of countries affected by the Coronavirus.
+    const getAffectedCountries = async () => {
+      await fetch("https://covid-193.p.rapidapi.com/countries", {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "covid-193.p.rapidapi.com",
+          "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => setCountries(data.response))
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getAffectedCountries();
+  }, []);
+
+  // onChange for dropdown menu
+  const onCountryChange = async (event) => {
+    const country = event.target.value;
+    setCountry(country);
+  };
+
+  return (
+    <div className="app">
+      {/* AppHeader component with props countries, country, onCountryChange */}
+      <AppHeader
+        countries={countries}
+        country={country}
+        onCountryChange={onCountryChange}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./AppHeader.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+
+export default function AppHeader({ countries, country, onCountryChange }) {
+  return (
+    <div className="appHeader">
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value={country} onChange={onCountryChange}>
+          <MenuItem value="worldWide">WorldWide</MenuItem>
+          {countries.map((country, index) => (
+            <MenuItem value={country} key={index}>
+              {country}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
