e8d08955ab037214c4b9c51be40399b95d8142c4
AKSHAY <akshayjadhav461999@gmail.com>
2020-08-31 17:16:36 +0530

    moved api calls from App.js to index.js

---
+++ src/api/index.js
@@ -0,0 +1,32 @@
+// getting list of countries affected by the Coronavirus.
+export const getAffectedCountries = () => {
+  return fetch("https://covid-193.p.rapidapi.com/countries", {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "covid-193.p.rapidapi.com",
+      "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => data.response)
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// get current status of the spread of the coronavirus
+export const getCountryData = (country) => {
+  const countryUrl = `https://covid-193.p.rapidapi.com/statistics?country=${country}`;
+  return fetch(countryUrl, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "covid-193.p.rapidapi.com",
+      "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => data.response[0])
+    .catch((err) => {
+      console.log(err);
+    });
+};
