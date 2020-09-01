a3b7b85d912cd8e7a2a6ce315021242cd55a4aef
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-01 13:59:01 +0530

    PieChart created

---
+++ src/components/PieChart/PieChart.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Pie } from "react-chartjs-2";
+function PieChart({ countryInfo }) {
+  return (
+    <div className="linearGraph">
+      <Pie
+        data={{
+          labels: ["cases", "deaths", "Recovered"],
+          datasets: [
+            {
+              data: [
+                countryInfo.cases?.total,
+                countryInfo.deaths?.total,
+                countryInfo.cases?.recovered,
+              ],
+              backgroundColor: ["#25CCF7", "#FF4848", "#45CE30"],
+            },
+          ],
+        }}
+        height={300}
+        options={{
+          responsive: false,
+        }}
+      />
+    </div>
+  );
+}
+
+export default PieChart;
