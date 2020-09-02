ef3214c72d2dba2f462253b6a68464c200c2f33f
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-02 14:17:34 +0530

    loading bar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import AppHeader from "./components/AppHeader/AppHeader";
-import { Card, CardContent, Grid, CardHeader } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  Grid,
+  CardHeader,
+  LinearProgress,
+} from "@material-ui/core";
 import { getAffectedCountries, getCountryData } from "./api";
 import StatusPanal from "./components/StatusPanal/StatusPanal";
 import StatTable from "./components/Table/StatTable";
@@ -17,6 +23,8 @@ function App() {
   // state for storing coronavirus status
   const [countryInfo, setCountryInfo] = useState({});
 
+  const [loading, setLoading] = useState(false);
+
   // getting list of countries affected by the Coronavirus.
   useEffect(() => {
     const getCountriesList = async () =>
@@ -26,68 +34,81 @@ function App() {
 
   //getting worldwide status to show when first time app loads
   useEffect(() => {
+    setLoading(true);
+
     const getWorldwideStatus = async () =>
       setCountryInfo(await getCountryData("all"));
     getWorldwideStatus();
+
+    setLoading(false);
   }, []);
 
   // onChange for dropdown menu
   const onCountryChange = async (event) => {
+    setLoading(true);
     const country = event.target.value;
     const getCountryStatus = await getCountryData(country);
     setCountry(country);
     setCountryInfo(getCountryStatus);
+    setLoading(false);
   };
 
   return (
-    <div className="app">
-      <div className="app__header">
-        {/* AppHeader component with props countries, country, onCountryChange */}
-        <AppHeader
-          countries={countries}
-          country={country}
-          onCountryChange={onCountryChange}
-        />
-      </div>
+    <>
+      {countryInfo && (
+        <div className="app">
+          {/* loader */}
+          {loading && <LinearProgress className="app__loader" />}
 
-      <div className="app__statusPanal">
-        {/* showing cases ,recoverd , deaths status  */}
-        <StatusPanal countryInfo={countryInfo} />
-      </div>
+          <div className="app__header">
+            {/* AppHeader component with props countries, country, onCountryChange */}
+            <AppHeader
+              countries={countries}
+              country={country}
+              onCountryChange={onCountryChange}
+            />
+          </div>
 
-      <div className="app__middleSection">
-        <Grid container spacing={3}>
-          {/* LineGraph */}
-          <Grid item xs={12} sm={12} md={8}>
-            <Card className="app__card">
-              <CardHeader title=" Coronavirus outbreak trend over time" />
+          <div className="app__statusPanal">
+            {/* showing cases ,recoverd , deaths status  */}
+            <StatusPanal countryInfo={countryInfo} />
+          </div>
+
+          <div className="app__middleSection">
+            <Grid container spacing={3}>
+              {/* LineGraph */}
+              <Grid item xs={12} sm={12} md={8}>
+                <Card className="app__card">
+                  <CardHeader title=" Coronavirus outbreak trend over time" />
+                  <CardContent>
+                    <LineGraph country={countryInfo?.country} />
+                  </CardContent>
+                </Card>
+              </Grid>
+              {/* Pie Chart */}
+              <Grid item xs={12} sm={12} md={4}>
+                <Card className="app__card">
+                  <CardHeader title="Summary of Total Deaths,Active,Recovered" />
+                  <CardContent>
+                    <PieChart countryInfo={countryInfo} />
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          </div>
+
+          <div className="app__statsTable">
+            <Card>
               <CardContent>
-                <LineGraph country={countryInfo.country} />
+                <h1>Current status of all countries</h1>
+                {/* showing stats for all counties in table form  */}
+                <StatTable />
               </CardContent>
             </Card>
-          </Grid>
-          {/* Pie Chart */}
-          <Grid item xs={12} sm={12} md={4}>
-            <Card className="app__card">
-              <CardHeader title="Summary of Total Deaths,Active,Recovered" />
-              <CardContent>
-                <PieChart countryInfo={countryInfo} />
-              </CardContent>
-            </Card>
-          </Grid>
-        </Grid>
-      </div>
-
-      <div className="app__statsTable">
-        <Card>
-          <CardContent>
-            <h1>Current status of all countries</h1>
-            {/* showing stats for all counties in table form  */}
-            <StatTable />
-          </CardContent>
-        </Card>
-      </div>
-    </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
