76f644d0606bc512e42cbc378d05021886b412ad
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-02 13:56:23 +0530

    check if data exist

--- src/utils/sortTableData.js
+++ src/utils/sortTableData.js
@@ -1,11 +1,15 @@
 export const sortTableData = (data) => {
-  const sortedData = [...data];
-  sortedData.sort((a, b) => {
-    if (a.cases.active > b.cases.active) {
-      return -1;
-    } else {
-      return 1;
-    }
-  });
-  return sortedData;
+  if (data) {
+    const sortedData = [...data];
+    sortedData.sort((a, b) => {
+      if (a.cases.active > b.cases.active) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    return sortedData;
+  } else {
+    return;
+  }
 };
