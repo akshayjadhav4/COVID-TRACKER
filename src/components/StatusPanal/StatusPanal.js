1c539cbac332f6923c20959b60e335fd2808987a
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-02 12:26:05 +0530

    title added

--- src/components/StatusPanal/StatusPanal.js
+++ src/components/StatusPanal/StatusPanal.js
@@ -2,28 +2,35 @@ import React from "react";
 import "./StatusPanal.css";
 import InfoBox from "../InfoBox/InfoBox";
 import { textPretty } from "../../utils/textPretty";
+import { Typography } from "@material-ui/core";
 
 function StatusPanal({ countryInfo }) {
   return (
     <div className="statusPanal">
-      <InfoBox
-        title="Active Cases"
-        cases={textPretty(countryInfo.cases?.active)}
-        newCases={textPretty(countryInfo.cases?.new)}
-      />
-      <InfoBox
-        title="Total Recoverd"
-        cases={textPretty(countryInfo.cases?.recovered)}
-      />
-      <InfoBox
-        title="Total Deaths"
-        cases={textPretty(countryInfo.deaths?.total)}
-        newCases={textPretty(countryInfo.deaths?.new)}
-      />
-      <InfoBox
-        title="Total cases"
-        cases={textPretty(countryInfo.cases?.total)}
-      />
+      <Typography variant="h5">
+        {countryInfo.country === "All" ? "Worldwide" : countryInfo.country}{" "}
+        Overview
+      </Typography>
+      <div className="statusPanal__cards">
+        <InfoBox
+          title="Active Cases"
+          cases={textPretty(countryInfo.cases?.active)}
+          newCases={textPretty(countryInfo.cases?.new)}
+        />
+        <InfoBox
+          title="Total Recoverd"
+          cases={textPretty(countryInfo.cases?.recovered)}
+        />
+        <InfoBox
+          title="Total Deaths"
+          cases={textPretty(countryInfo.deaths?.total)}
+          newCases={textPretty(countryInfo.deaths?.new)}
+        />
+        <InfoBox
+          title="Total cases"
+          cases={textPretty(countryInfo.cases?.total)}
+        />
+      </div>
     </div>
   );
 }
